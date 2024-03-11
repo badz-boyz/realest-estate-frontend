78f33c297714240b816d1a5f5b04955f9d83af56
Armando Bugarin <armando.bugarin24@gmail.com>
2024-03-11 15:34:56 -0700

    add logo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import logo from '../public/logo.png';
 
 export default function Home() {
   return (
     <main>
       coming soon
+      <Image src={logo} alt="realest estate logo" className="logo" />
     </main>
   );
 }
