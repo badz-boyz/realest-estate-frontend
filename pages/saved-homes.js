77b1775d952b4a2de3c7c88f2ab1ae4f7abf0e51
Armando Bugarin <armando.bugarin24@gmail.com>
2024-03-13 22:04:06 -0700

    edit saved-homes.js

---
+++ pages/saved-homes.js
@@ -0,0 +1,10 @@
+import Card from "../components/Card";
+
+export default function SavedHomesPage() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <p style={{ fontSize: "24px", marginBottom: '20px' }}>Your saved homes are displayed below</p>
+      <Card />
+    </div>
+  );
+}
