0cc434d11be4e3f56ed3aaacb2e3282a709acd2b
HighMid <xstar671@gmail.com>
2024-03-14 19:55:22 -0400

    Updated authcontext

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,36 +1,37 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext(null);
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
 
-const [user, setUser] = useState(() => {
-  try {
-    const savedUser = localStorage.getItem('user');
-    return savedUser ? JSON.parse(savedUser) : null;
-  } catch (error) {
-    console.error("Error parsing user from localStorage", error);
-    return null;
-  }
-});
+    useEffect(() => {
+        // Only attempt to fetch user from localStorage if we're on the client side
+        const savedUser = typeof window !== 'undefined' ? localStorage.getItem('user') : null;
+        if (savedUser) {
+            setUser(JSON.parse(savedUser));
+        }
+    }, []);
 
+    const login = (userData) => {
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('user', JSON.stringify(userData));
+        }
+        setUser(userData);
+    };
 
-  const login = (userData) => {
-    localStorage.setItem('user', JSON.stringify(userData));
-    setUser(userData);
-  };
-  
+    const logout = () => {
+        if (typeof window !== 'undefined') {
+            localStorage.removeItem('user');
+        }
+        setUser(null);
+    };
 
-  const logout = () => {
-    localStorage.removeItem('user');
-    setUser(null);
-  };
-
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
